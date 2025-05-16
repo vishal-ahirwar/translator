2eb0a555076b5ec7623c01940c0cc0295fd213e5
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-05-16 19:42:17 +0530

    scripts added

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,3 @@
 #flick
 import os
-current_path = os.getcwd()
 os.system("cd backend && uvicorn main:app --host 0.0.0.0 --port 8000")
--- frontend/run.py
+++ frontend/run.py
@@ -1,6 +1,2 @@
-#flick
 import os
-import subprocess
-import webbrowser
-subprocess.Popen(["python", "-m", "http.server", "5500"], cwd="frontend")
-webbrowser.open("http://localhost:5500")
+os.system("cd frontend && python -m http.server 5500")
